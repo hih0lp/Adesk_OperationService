329b0524c74058c1e81df28242c12f70dde95bff
hih0lp <mrlibobo@yandex.ru>
2026-02-10 17:21:35 +0300

    -

--- src/main/java/Adesk_OperationService/Controllers/RequestControllerAdvice.java
+++ src/main/java/Adesk_OperationService/Controllers/RequestControllerAdvice.java
@@ -1,14 +1,16 @@
-//package Adesk_OperationService.Controllers;
-//
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.ExceptionHandler;
-//import org.springframework.web.bind.annotation.RestControllerAdvice;
-//
-//@RestControllerAdvice
-//public class RequestControllerAdvice {
-//
-//    @ExceptionHandler(Exception.class)
-//    public ResponseEntity<String> badRequest(Exception e){
-//        return ResponseEntity.badRequest().body(e.getMessage());
-//    }
-//}
+package Adesk_OperationService.Controllers;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RequestControllerAdvice {
+
+    @ExceptionHandler(Exception.class)
+    @Hidden
+    public ResponseEntity<String> badRequest(Exception e){
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+}
