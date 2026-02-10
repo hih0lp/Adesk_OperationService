1c05741304726fa76f366823c94baa42aff913c1
hih0lp <mrlibobo@yandex.ru>
2026-02-10 17:24:42 +0300

    -

--- src/main/java/Adesk_OperationService/Controllers/RequestControllerAdvice.java
+++ src/main/java/Adesk_OperationService/Controllers/RequestControllerAdvice.java
@@ -1,11 +1,17 @@
 package Adesk_OperationService.Controllers;
 
 import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@ConditionalOnProperty(
+        name = "springdoc.api-docs.enabled",
+        havingValue = "false",
+        matchIfMissing = true
+)
 public class RequestControllerAdvice {
 
     @ExceptionHandler(Exception.class)
